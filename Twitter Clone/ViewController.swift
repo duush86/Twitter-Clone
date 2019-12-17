83eea0d770341317f96c5c5d6d164f46002f0ea6
Antonio <j.antonio.oro@gmail.com>
2019-12-17 15:23:16 -0600

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Antonio Orozco on 12/17/19.
+//  Copyright © 2019 Antonio Orozco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
